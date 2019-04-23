da04a72cb2f2755d7862e444577b5ca463bbe962
Shosh Seiden <shosh.j.seiden@gmail.com>
2019-04-22 21:18:49 -0600

    Set up pages

---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>SJSeiden's Websites</h1>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Sites.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Sites</h1>
+</template>
+
+<script>
+export default {
+  name: 'Sites'
+}
+</script>
+
+<style>
+</style>
