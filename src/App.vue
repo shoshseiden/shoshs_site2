585da9484a8b2780bb8633987d462228bbd4c341
Shosh Seiden <shosh.j.seiden@gmail.com>
2019-04-22 21:51:37 -0600

    navigation creation

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,17 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
+    <Navigation></Navigation>
   </div>
 </template>
 
 <script>
+import Navigation from './components/Navigation'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    'Navigation': Navigation
+  }
 }
 </script>
 
---
+++ src/components/Navigation.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h2>Nav Bar</h2>
+    <nav>
+      <router-link class="spacing" v-for="routes in links" v-bind:key="routes.id":to="`${routes.page}`">{{routes.text}}</router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default{
+  data() {
+    return {
+      links: [
+        {
+          id: 0,
+          text: 'Hello World',
+          page: '/HelloWorld'
+        },
+        {
+          id: 1,
+          text: 'Home',
+          page: '/Home'
+        },
+        {
+          id: 2,
+          text: 'Sites',
+          page: '/Sites'
+        },
+        {
+          id: 3,
+          text: 'Contact',
+          page: '/Contact'
+        },
+        {
+          id: 4,
+          text: 'Skills',
+          page: '/Skills'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+  .spacing {
+    margin-right: 10px;
+  }
+</style>
