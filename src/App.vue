fefa8af5615de372fa6963f3894310d740599dda
Shosh Seiden <shosh.j.seiden@gmail.com>
2019-04-25 21:41:16 -0600

    background image

--- src/App.vue
+++ src/App.vue
@@ -17,11 +17,6 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-image: url('assets/html_img.png');
 }
 </style>
