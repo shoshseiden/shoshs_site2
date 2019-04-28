c3bc2a8277b4fb4d2adcb76a11cd62b21370e761
Shosh Seiden <shosh.j.seiden@gmail.com>
2019-04-27 21:08:50 -0600

    Added skills data

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,12 +1,28 @@
 <template>
   <div id="skills">
-    <h1>Skills</h1>
+    <h1>Web Development Skills</h1>
+    <ul v-for="skill in skills">
+      <li>{{ skill.name }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Skills'
+  name: 'Skills',
+  data(){
+    return{
+      skills: [
+        {name: 'HTML'},
+        {name: 'CSS'},
+        {name: 'JavaScript'},
+        {name: 'JQuery'},
+        {name: 'VueJS'},
+        {name: 'Django'},
+        {name: 'Python'},
+      ]
+    }
+  }
 }
 </script>
 
